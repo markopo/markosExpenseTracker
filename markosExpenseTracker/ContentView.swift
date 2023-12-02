3b8fc5ac79ed15901c93ad8a4fc5269a1b35d4b3
markopo <marko_poikkimaki@hotmail.com>
2023-12-02 16:33:26 +0100

    Update ContentView.swift

--- markosExpenseTracker/ContentView.swift
+++ markosExpenseTracker/ContentView.swift
@@ -32,10 +32,45 @@ struct SecondView: View {
     }
 }
 
+struct ThirdView: View {
+    @Environment(\.dismiss) var dismiss
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+    }
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+                
+                Button("Dismiss") {
+                    dismiss()
+                }
+            }.toolbar {
+                EditButton()
+            }
+            Spacer()
+        }
+    }
+}
+
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    
     @State private var showingSheet = false
+    @State private var showList = false
     
 
     var body: some View {
@@ -47,6 +82,12 @@ struct ContentView: View {
             }
             .sheet(isPresented: $showingSheet, content: {
                 SecondView(name: "Marko")
+            })
+            Button("Show list") {
+                showList.toggle()
+            }
+            .sheet(isPresented: $showList, content: {
+                ThirdView()
             })
         }
         Spacer()
